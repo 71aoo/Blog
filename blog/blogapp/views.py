a38db8f5b7c219e287b0d6d2073688798feb06b5
playwin <779244202@qq.com>
2019-05-09 12:23:28 +0800

    20190509

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -8,6 +8,7 @@ import markdown
 
 # 首页
 def index(request):
+    tags = []
     user = User.objects.first()
     navbar = Navbar.objects.all() #顶部导航栏
     article = Article.objects.all().order_by('-create_time') # 文章列表
@@ -23,34 +24,48 @@ def index(request):
     except PageNotAnInteger:
         article = page_robot.page(1)
 
+    for arti in article:
+        for tag in arti.tags.all():
+            if tag not in tags:
+                tags.append(tag)
+    
     context = {
         'navbar': navbar ,
         'article' : article ,
         'recommend' : recommend,
         'pagerange' : page_robot.page_range,
         'one' : one ,
-        'user' : user
+        'user' : user,
+        'tags' : tags
     }
     return render(request,'index.html',context)
 
 # 关于我
 def about(request):
+    tags = []
     tag_num = 0
     user = User.objects.get(id=1)
     header_img = Header_img.objects.get(id=1)
     navbar = Navbar.objects.all()
     arti_num = Article.objects.count()
     article = Article.objects.all()
+    
     for art in article:
         tag_num = tag_num + art.tags.count()
+        for tag in art.tags.all():
+            if tag not in tags:
+                tags.append(tag)
+
     context = {
         'navbar': navbar ,
         'user': user,
         'article': article,
         'arti_num' : arti_num,
         'tag_num':tag_num,
-        'header_img' : header_img
+        'header_img' : header_img,
+        'tags' : tags
     }
+    
     return render(request,'about.html',context)
 
 # 大佬在这
@@ -69,15 +84,23 @@ def friends(request):
 
 # 归档
 def sort(request):
+    tags = []
     header_img = Header_img.objects.get(id=1)
     user = User.objects.get(id=1)
     navbar = Navbar.objects.all()
-    dates =  Article.objects.all().order_by('-create_time')   
+    dates =  Article.objects.all().order_by('-create_time')
+
+    for arti in dates:
+        for tag in arti.tags.all():
+            if tag not in tags:
+                tags.append(tag)
+    
     context = {}
     context['navbar'] = navbar
     context['dates'] = dates
     context['user'] = user
-    context['header_img'] = header_img 
+    context['header_img'] = header_img
+    context['tags'] = tags 
     return render(request,'sort.html',context)
 
 def detail(request, id):

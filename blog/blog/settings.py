32763a97fa5284bcaf81285422ad96a137908eb6
playwi0 <779244202@qq.com>
2019-10-20 12:35:26 +0800

    20191020

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static").replace("//","/")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/static/"),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media').replace("//","/")
